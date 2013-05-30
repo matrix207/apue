542a8733ca9eff4d5f963a14046041f8d7b3c1f6
dennis <dennis.cpp@gmail.com>
2013-05-30 17:50:22 +0800

    daemon program

---
+++ daemon/daemonize.c
@@ -0,0 +1,100 @@
+#include "apue.h"
+#include <syslog.h>
+#include <fcntl.h>
+#include <sys/resource.h>
+
+void
+daemonize(const char *cmd)
+{
+	int i, fd0, fd1, fd2;
+	pid_t pid;
+	struct rlimit rl;
+	struct sigaction sa;
+
+	/*
+	 * Clear file creation mask.
+	 */
+	umask(0);
+
+	/*
+	 * Get maximum number of file descriptors.
+	 */
+	if (getrlimit(RLIMIT_NOFILE, &rl) < 0)
+		err_quit("%s: can't get file limit", cmd);
+	
+	/*
+	 * Become a session leader to lose controlling TTY
+	 */
+	if ((pid = fork()) < 0)
+		err_quit("%s: can't fork", cmd);
+	else if (pid != 0) /* parent */
+		exit(0);
+	setsid();
+
+	printf("After first fork.\n");
+	/*
+	 * Ensure future opens won't allocate controlling TTY
+	 */
+	sa.sa_handler = SIG_IGN;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	if (sigaction(SIGHUP, &sa, NULL) < 0)
+		err_quit("%s: can't ignore SIGHUP");
+	if ((pid = fork()) < 0)
+		err_quit("%s: can't fork", cmd);
+	else if (pid != 0) /* parent */
+		exit(0);
+	
+	printf("After second fork.\n");
+	/*
+	 * Change the current working directory to the root so
+	 * we won't prevent file systems from being unmounted.
+	 */
+	if (chdir("/") < 0)
+		err_quit("%s: can't change directory to /");
+	
+	/*
+	 * close all open file descriptors
+	 */
+	if (rl.rlim_max == RLIM_INFINITY)
+		rl.rlim_max = 1024;
+	for (i=0; i<rl.rlim_max; i++)
+		close(i);
+	
+	/*
+	 * Attach file descriptors 0, 1 and 2 to /dev/null 
+	 */
+	fd0 = open("/dev/null", O_RDWR);
+	fd1 = dup(0);
+	fd2 = dup(0);
+
+	/*
+	 * Initialize the log file.
+	 */
+	openlog(cmd, LOG_CONS, LOG_DAEMON);
+	if (fd0 != 0 || fd1 != 1 || fd2 != 2) {
+		syslog(LOG_ERR, "unexpected file descriptors %d %d %d", fd0, fd1, fd2);
+		exit(1);
+	}
+}
+
+/*
+ * Caller must define and set this: nonzero if interactive, zero if daemons
+ */
+int log_to_stderr = 0;
+
+int
+main(int argc, char **argv)
+{
+	/* run as daemon */
+	daemonize("daemontest");
+
+	printf("do while loop.\n");
+	while (1) {
+		/* do your job here */
+
+		sleep(10);
+	}
+
+	return 0;
+}
